d2f64a0de0a01f54724dd47179ec5fb615e6cde1
JershonEspitia <jershonespitiarey@gmail.com>
2023-10-13 09:46:53 -0500

    feat: :sparkles: Se agregan funciones al app.js
    Se agregaron las funciones de actionBtnAbrigos, actionBtnCamisetas, actionBtnPantalon

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 import { getOne } from "./storage/methods.js";
 import { endPointAbrigo, endPointCamiseta, endPointPantalon, endPointCarro } from "./storage/endPointsProducts.js"
 
+let carritoCompra = [];
+
 addEventListener("DOMContentLoaded", async() => {
     let btnTodos = document.querySelector("#todosProducts");
     let btnAbrigos = document.querySelector("#abrigos");
@@ -14,10 +16,14 @@ addEventListener("DOMContentLoaded", async() => {
     // console.log( await getOne({endPoint: endPointCarro}));
     
     actionBtnTodos(btnTodos);
+    actionBtnAbrigos(btnAbrigos);
+    actionBtnCamisetas(btnCamisetas);
+    actionBtnPantalon(btnPantalones);
 });
 
 let actionBtnTodos = (btnTodos)=>{
     btnTodos.addEventListener("click", async()=>{
+
         let titleSection = document.querySelector("#titleSection");
         titleSection.textContent = `${btnTodos.textContent}`;
 
@@ -37,6 +43,7 @@ let actionBtnTodos = (btnTodos)=>{
         });
         
         let infoData = document.querySelector(".info-data");
+        infoData.innerHTML = ""
         allProducts.forEach(element => {
             infoData.insertAdjacentHTML(
                 "beforeend",
@@ -56,6 +63,106 @@ let actionBtnTodos = (btnTodos)=>{
                 `
             );
         });
-        
+        myButtons();
     });
 };
+
+let actionBtnAbrigos = (btnAbrigos)=>{
+    btnAbrigos.addEventListener("click", async()=>{
+
+        let titleSection = document.querySelector("#titleSection");
+        titleSection.textContent = `${btnAbrigos.textContent}`;
+
+        let resAbrigo = await getOne({endPoint: endPointAbrigo});
+        
+        let infoData = document.querySelector(".info-data");
+        infoData.innerHTML = ""
+        resAbrigo.forEach(element => {
+            infoData.insertAdjacentHTML(
+                "beforeend",
+                `
+                <div class="card">
+                    <div class="headProduct">
+                        <img class="imageProduct" src="${element.imagen}" alt="producto">
+                    </div>
+                    <div class="bodyCard">
+                        <div class="namePriceProduct">
+                            <h3 class="titleProduct">${element.nombre}</h3>
+                            <h5 class="priceProduct">$ ${element.precio}</h5>
+                        </div>
+                        <a href="#" class="myButton">Agregar</a>
+                    </div>
+                </div>
+                `
+            );
+        });
+        myButtons();
+    });
+};
+
+let actionBtnCamisetas = (btnCamisetas)=>{
+    btnCamisetas.addEventListener("click", async()=>{
+
+        let titleSection = document.querySelector("#titleSection");
+        titleSection.textContent = `${btnCamisetas.textContent}`;
+
+        let resCamiseta = await getOne({endPoint: endPointCamiseta});
+        
+        let infoData = document.querySelector(".info-data");
+        infoData.innerHTML = ""
+        resCamiseta.forEach(element => {
+            infoData.insertAdjacentHTML(
+                "beforeend",
+                `
+                <div class="card">
+                    <div class="headProduct">
+                        <img class="imageProduct" src="${element.imagen}" alt="producto">
+                    </div>
+                    <div class="bodyCard">
+                        <div class="namePriceProduct">
+                            <h3 class="titleProduct">${element.nombre}</h3>
+                            <h5 class="priceProduct">$ ${element.precio}</h5>
+                        </div>
+                        <a href="#" class="myButton">Agregar</a>
+                    </div>
+                </div>
+                `
+            );
+        });
+        myButtons();
+    });
+};
+
+let actionBtnPantalon = (btnPantalones)=>{
+    btnPantalones.addEventListener("click", async()=>{
+
+        let titleSection = document.querySelector("#titleSection");
+        titleSection.textContent = `${btnPantalones.textContent}`;
+
+        let resPantalon = await getOne({endPoint: endPointPantalon});
+        
+        let infoData = document.querySelector(".info-data");
+        infoData.innerHTML = ""
+        resPantalon.forEach(element => {
+            infoData.insertAdjacentHTML(
+                "beforeend",
+                `
+                <div class="card">
+                    <div class="headProduct">
+                        <img class="imageProduct" src="${element.imagen}" alt="producto">
+                    </div>
+                    <div class="bodyCard">
+                        <div class="namePriceProduct">
+                            <h3 class="titleProduct">${element.nombre}</h3>
+                            <h5 class="priceProduct">$ ${element.precio}</h5>
+                        </div>
+                        <a href="#" class="myButton">Agregar</a>
+                    </div>
+                </div>
+                `
+            );
+        });
+        myButtons();
+    });
+};
+
